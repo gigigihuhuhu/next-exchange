28c6329394f448b9910bc5e3f609bbfd3905a8fe
“kyungsu” <kyungsu77777@gmail.com>
2024-12-06 18:07:38 +0900

    footer 샘플데이터 추가

---
+++ src/data/sample-data.tsx
@@ -0,0 +1,41 @@
+export const footerData:{
+  category: string;
+  links: { label: string; href: string }[];
+}[] = [
+  {
+    category: "안내",
+    links: [
+      { label: "회사소개", href: "#" },
+      { label: "공지사항", href: "#" },
+      { label: "거래 이용 안내", href: "#" },
+      { label: "입출금 이용 안내", href: "#" },
+    ],
+  },
+  {
+    category: "약관 및 정책",
+    links: [
+      { label: "업비트 이용약관", href: "#" },
+      { label: "Open API 이용약관", href: "#" },
+      { label: "개인정보처리방침", href: "#" },
+      { label: "청소년보호정책", href: "#" },
+    ],
+  },
+  {
+    category: "고객지원",
+    links: [
+      { label: "자주하는 질문", href: "#" },
+      { label: "1:1 문의하기", href: "#" },
+      { label: "카카오톡 상담하기", href: "#" },
+      { label: "Open API", href: "#" },
+      { label: "거래지원 문의 및 제보", href: "#" },
+    ],
+  },
+  {
+    category: "연관 서비스",
+    links: [
+      { label: "프라이버시센터", href: "#" },
+      { label: "투자자보호센터", href: "#" },
+      { label: "버그바운티 프로그램", href: "#" },
+    ],
+  },
+];
