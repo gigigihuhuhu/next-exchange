a9bb8469789e4ef58dc121559c1d7d8b8c86c62f
“kyungsu” <kyungsu77777@gmail.com>
2024-12-10 14:26:01 +0900

    Exchange 페이지 Layout 정리

--- src/app/exchange/page.tsx
+++ src/app/exchange/page.tsx
@@ -1,7 +1,26 @@
+import LwWidget from "@/components/ui/lw-widget";
+
 export default function Home() {
   return (
-    <>
-      <h1>Exchange</h1>
-    </>
+    <div className="bg-gray-200 grid grid-cols-3 gap-2 py-2">
+      <div className="col-span-2 flex flex-col gap-2">
+        <div className="bg-white h-[50px]"></div>
+        <div>
+          <div className="bg-white h-[50px]"></div>
+          <div className="bg-white h-[400px]">
+            <LwWidget></LwWidget>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          <div className="bg-white"></div>
+          <div className="flex flex-col gap-2">
+            <div className="bg-white h-[50px]"></div>
+            <div className="bg-white h-[50px]"></div>
+          </div>
+        </div>
+        <div className="bg-white h-[50px]"></div>
+      </div>
+      <div className="bg-white col-span-1"></div>
+    </div>
   );
 }
