24b165a8518fb74b1a2462ceb16d8f02c83c73f2
“kyungsu” <kyungsu77777@gmail.com>
2024-12-10 22:46:35 +0900

    거래소 Link에 쿼리 파라미터 추가 (default KRW-BTC)

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,13 +4,18 @@ import NavLink from "@/components/ui/nav-link";
 import AuthButton from "@/components/ui/auth-button";
 
 export default function Header() {
+  const defaultMarket = "KRW-BTC";
   return (
     <header className="fixed flex items-center z-10 w-full bg-blue-900 h-12 py-8 justify-between px-10">
       <Link className="grow-0" href="/">
         <Logo></Logo>
       </Link>
       <div className="grow flex flex-row gap-10 items-center justify-center text-slate-400 font-semibold">
-        <NavLink href="/exchange">거래소</NavLink>
+        <NavLink
+          href={{ pathname: "/exchange", query: { code: defaultMarket } }}
+        >
+          거래소
+        </NavLink>
         <NavLink href="/balances">입출금</NavLink>
         <NavLink href="/investments">투자내역</NavLink>
       </div>
--- src/components/ui/nav-link.tsx
+++ src/components/ui/nav-link.tsx
@@ -3,19 +3,22 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
+interface NavLinkProps extends React.ComponentProps<typeof Link> {
+  children: React.ReactNode;
+}
+
 export default function NavLink({
   href,
   children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
+  ...props
+}: NavLinkProps) {
   const pathname = usePathname();
-  const isActive = pathname === href;
+  const isActive = pathname === (typeof href === "string" ? href : href.pathname);
 
   return (
     <Link
       href={href}
+      {...props}
       className={isActive ? "text-white font-extrabold" : "text-slate-400"}
     >
       {children}
