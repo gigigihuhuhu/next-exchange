606830ffc3acc7812ebcb45e200945ca5000561a
“kyungsu” <kyungsu77777@gmail.com>
2024-12-11 14:20:55 +0900

    Coin Class 리펙토링

--- src/model/coin.ts
+++ src/model/coin.ts
@@ -30,60 +30,64 @@ interface CoinDTO {
 }
 
 export class Coin {
-  market: string;
-  tradeDate: string;
-  tradeTime: string;
-  tradeDateKst: string;
-  tradeTimeKst: string;
-  tradeTimestamp: number;
-  openingPrice: number;
-  highPrice: number;
-  lowPrice: number;
-  tradePrice: number;
-  prevClosingPrice: number;
-  change: string;
-  changePrice: number;
-  changeRate: number;
-  signedChangePrice: number;
-  signedChangeRate: number;
-  tradeVolume: number;
-  accTradePrice: number;
-  accTradePrice24h: number;
-  accTradeVolume: number;
-  accTradeVolume24h: number;
-  highest52WeekPrice: number;
-  highest52WeekDate: string;
-  lowest52WeekPrice: number;
-  lowest52WeekDate: string;
-  timestamp: number;
+  constructor(
+    public market: string,
+    public tradeDate: string,
+    public tradeTime: string,
+    public tradeDateKst: string,
+    public tradeTimeKst: string,
+    public tradeTimestamp: number,
+    public openingPrice: number,
+    public highPrice: number,
+    public lowPrice: number,
+    public tradePrice: number,
+    public prevClosingPrice: number,
+    public change: string,
+    public changePrice: number,
+    public changeRate: number,
+    public signedChangePrice: number,
+    public signedChangeRate: number,
+    public tradeVolume: number,
+    public accTradePrice: number,
+    public accTradePrice24h: number,
+    public accTradeVolume: number,
+    public accTradeVolume24h: number,
+    public highest52WeekPrice: number,
+    public highest52WeekDate: string,
+    public lowest52WeekPrice: number,
+    public lowest52WeekDate: string,
+    public timestamp: number
+  ) {}
 
-  constructor(data: CoinDTO) {
-    this.market = data.market;
-    this.tradeDate = data.trade_date;
-    this.tradeTime = data.trade_time;
-    this.tradeDateKst = data.trade_date_kst;
-    this.tradeTimeKst = data.trade_time_kst;
-    this.tradeTimestamp = data.trade_timestamp;
-    this.openingPrice = data.opening_price;
-    this.highPrice = data.high_price;
-    this.lowPrice = data.low_price;
-    this.tradePrice = data.trade_price;
-    this.prevClosingPrice = data.prev_closing_price;
-    this.change = data.change;
-    this.changePrice = data.change_price;
-    this.changeRate = data.change_rate;
-    this.signedChangePrice = data.signed_change_price;
-    this.signedChangeRate = data.signed_change_rate;
-    this.tradeVolume = data.trade_volume;
-    this.accTradePrice = data.acc_trade_price;
-    this.accTradePrice24h = data.acc_trade_price_24h;
-    this.accTradeVolume = data.acc_trade_volume;
-    this.accTradeVolume24h = data.acc_trade_volume_24h;
-    this.highest52WeekPrice = data.highest_52_week_price;
-    this.highest52WeekDate = data.highest_52_week_date;
-    this.lowest52WeekPrice = data.lowest_52_week_price;
-    this.lowest52WeekDate = data.lowest_52_week_date;
-    this.timestamp = data.timestamp;
+  static fromApiData(data: CoinDTO): Coin {
+    return new Coin(
+      data.market,
+      data.trade_date,
+      data.trade_time,
+      data.trade_date_kst,
+      data.trade_time_kst,
+      data.trade_timestamp,
+      data.opening_price,
+      data.high_price,
+      data.low_price,
+      data.trade_price,
+      data.prev_closing_price,
+      data.change,
+      data.change_price,
+      data.change_rate,
+      data.signed_change_price,
+      data.signed_change_rate,
+      data.trade_volume,
+      data.acc_trade_price,
+      data.acc_trade_price_24h,
+      data.acc_trade_volume,
+      data.acc_trade_volume_24h,
+      data.highest_52_week_price,
+      data.highest_52_week_date,
+      data.lowest_52_week_price,
+      data.lowest_52_week_date,
+      data.timestamp
+    );
   }
 
   currencyType(): string {
@@ -97,9 +101,5 @@ export class Coin {
   coinName(locale: string) {
     const coinCode = this.coinCode();
     return coinCodeToNameMapper[coinCode][locale as "en" | "kr"];
-  }
-
-  static fromApiData(data: CoinDTO): Coin {
-    return new Coin(data);
   }
 }
