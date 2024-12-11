97eab606caa3fcd9be88e94c3c6cacc14e496637
“kyungsu” <kyungsu77777@gmail.com>
2024-12-12 02:53:23 +0900

    make it sticky

--- src/components/ui/market-grid.tsx
+++ src/components/ui/market-grid.tsx
@@ -13,23 +13,46 @@ export function MarketGrid({ markets }: { markets: string }) {
   ];
 
   return (
-    <div>
-      <div className="flex flex-row items-start justify-between *:w-full *:font-semibold *:py-1 *:transition-colors *:duration-200">
-        {currencyTypes.map((currencyType, index) => {
-          return (
-            <button
-              key={index}
-              className={
-                activeCurrencyIdx === index
-                  ? "text-blue-600 border-b-4 border-blue-600"
-                  : "hover:border-b-4 hover:border-gray-400"
-              }
-              onClick={() => setActiveCurrencyIdx(index)}
-            >
-              {currencyType.koreanName}
-            </button>
-          );
-        })}
+    <>
+      <div className="sticky top-0 bg-white z-[5]">
+        <div className="flex flex-row items-start justify-between *:w-full *:font-semibold *:py-1 *:transition-colors *:duration-200">
+          {currencyTypes.map((currencyType, index) => {
+            return (
+              <button
+                key={index}
+                className={
+                  activeCurrencyIdx === index
+                    ? "text-blue-600 border-b-4 border-blue-600"
+                    : "hover:border-b-4 hover:border-gray-400"
+                }
+                onClick={() => setActiveCurrencyIdx(index)}
+              >
+                {currencyType.koreanName}
+              </button>
+            );
+          })}
+        </div>
+        <div
+          className={
+            "flex flex-row items-center px-3 border-t h-[30px] w-full bg-gray-50 text-[0.72rem] font-medium"
+          }
+        >
+          <div className="basis-[30px]">
+            <h4 className="text-gray-500"></h4>
+          </div>
+          <div className="basis-[98px] text-left">
+            <h4 className="text-gray-500">한글명</h4>
+          </div>
+          <div className="basis-[94px] text-center">
+            <h4 className="text-gray-500">현재가</h4>
+          </div>
+          <div className="basis-[58px] text-center">
+            <h4 className="text-gray-500">전일대비</h4>
+          </div>
+          <div className="basis-[98px] text-right">
+            <h4 className="text-gray-500">거래대금</h4>
+          </div>
+        </div>
       </div>
       <div className="relative w-full *:w-full">
         {currencyTypes.map((currencyType, index) => {
@@ -48,6 +71,6 @@ export function MarketGrid({ markets }: { markets: string }) {
           );
         })}
       </div>
-    </div>
+    </>
   );
 }
